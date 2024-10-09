a86ac2a1b6a7e0b7e25b4dfd83508ccd66ce33fb
NguyenTheLoc-chanh <22a1001d0199@students.hou.edu.com>
2024-10-09 21:01:37 +0700

    -Login

--- assets/js/home.js
+++ assets/js/home.js
@@ -6,3 +6,53 @@ document.addEventListener("DOMContentLoaded",function(){
         mobileNav.classList.toggle('active')
     })
 })
+
+// Login and Register
+const modal = document.querySelector(".modal");
+const modalBody = document.querySelector(".modal__body");
+const formLogin = document.querySelector(".auth-form-login");
+const formRegister = document.querySelector(".auth-form-register");
+const login = document.querySelector(".header__navbar-user-name.login");
+const register = document.querySelector(".header__navbar-user-name.register")
+
+login.addEventListener("click", function(e){
+    e.preventDefault();
+    modal.classList.add("active");
+    if(formRegister.classList.contains("active")){
+        formRegister.classList.remove("active");
+    }
+    formLogin.classList.add("active");
+})
+
+register.addEventListener("click", function(e){
+    e.preventDefault();
+    modal.classList.add("active");
+    if(formLogin.classList.contains("active")){
+        formLogin.classList.remove("active");
+    }
+    formRegister.classList.add("active");
+})
+
+modalBody.addEventListener("click", function(e){
+    e.preventDefault();
+    //e.target là phần tử mà người dùng đã nhấn vào. trả về true
+    if (!formLogin.contains(e.target) && !formRegister.contains(e.target)) {
+        modal.classList.remove("active");
+    }
+})
+
+// Đăng nhập khi ấn vào form đăng ký
+const loginForm = document.querySelector(".auth-form__switch-btn");
+const registerForm = document.querySelector(".auth-form__switch-btn.registerForm");
+
+loginForm.addEventListener("click", function(e){
+    e.preventDefault();
+    formRegister.classList.remove("active");
+    formLogin.classList.add("active");
+})
+
+registerForm.addEventListener("click", function(e){
+    e.preventDefault();
+    formLogin.classList.remove("active");
+    formRegister.classList.add("active");
+})
